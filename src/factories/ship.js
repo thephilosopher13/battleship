6575b9a9743d1a69aba04cc5ca50196759a2cfea
thephilosopher13 <60373194+thephilosopher13@users.noreply.github.com>
2023-08-03 18:09:16 +0800

    finish shipFactory

---
+++ src/factories/ship.js
@@ -0,0 +1,66 @@
+const shipModule = (() => {
+
+    const hitboxFactory = () => {
+
+        const setCoordinates = function(coordinatesValue) {
+            this.coordinates = coordinatesValue
+        }
+
+        const changeIsHit = function() {
+            this.isHit = true
+        }
+
+        return {
+            coordinates: null,
+            isHit: false,
+            setCoordinates,
+            changeIsHit
+        }
+    }
+
+
+    const shipFactory = (numberOfHitboxes) => {
+        const ship = {
+            length: numberOfHitboxes,
+            hits: 0,
+            isSunk: false,
+        };
+        
+        for (let i = 1; i <= numberOfHitboxes; i++) {
+            const hitbox = hitboxFactory()
+            ship[`hitbox${i}`] = hitbox;
+        }
+
+        const sinkShip = function() {
+            ship.isSunk = true
+        }
+
+        ship.addHit = function() {
+            // Loop through each hitbox and check if it's hit
+            for (let i = 1; i <= numberOfHitboxes; i++) {
+                if (this[`hitbox${i}`].isHit) {
+                    this.hits += 1;
+                    if (this.hits === this.length) {
+                        sinkShip()
+                    }
+                }
+            }
+        }
+        return ship
+    }
+
+    return {
+        shipFactory
+    };
+})();
+
+export default shipModule
+
+
+/*
+
+1. create 'hitbox' factory with coordinates and isHit as the properties (with isHit initally false)
+1.1. make a method inside hitboxFactory called 'setCoordinates'
+
+1. create a factory function 'shipFactory' that has number of hitboxes as arguments
+*/
